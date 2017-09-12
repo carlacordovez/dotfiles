1faad182ab924922672b0096b3b30d88cafec751
James Trinklein <james.trinklein@changepoint.com>
2017-09-12 13:20:47 -0700

    fix: some corrections for ppm setup scripts

--- projects/ppm/add-host-entries.sh
+++ projects/ppm/add-host-entries.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 set -e
 
-sudo hostess update localvm.daptiv.com $PPM_DEV_VM_IP
-sudo hostess update dev.daptiv.com $PPM_DEV_VM_IP
-sudo hostess update devadmin.daptiv.com $PPM_DEV_VM_IP
-sudo hostess update devapi.daptiv.com $PPM_DEV_VM_IP
-sudo hostess update devauth.daptiv.com $PPM_DEV_VM_IP
-sudo hostess update devsso.daptiv.com $PPM_DEV_VM_IP
-sudo hostess update devppm.daptiv.com 127.0.0.1
+sudo hostess add localvm.daptiv.com $PPM_DEV_VM_IP
+sudo hostess add dev.daptiv.com $PPM_DEV_VM_IP
+sudo hostess add devadmin.daptiv.com $PPM_DEV_VM_IP
+sudo hostess add devapi.daptiv.com $PPM_DEV_VM_IP
+sudo hostess add devauth.daptiv.com $PPM_DEV_VM_IP
+sudo hostess add devsso.daptiv.com $PPM_DEV_VM_IP
+sudo hostess add devppm.daptiv.com 127.0.0.1
