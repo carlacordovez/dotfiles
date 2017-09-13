af98f4bcc2c2ffd6d432d477f3267c2206bad1e8
James Trinklein <james.trinklein@changepoint.com>
2017-09-13 15:50:55 -0700

    fix: do not delete dev_ppm folder on vm setup

--- projects/ppm/setup-vm.sh
+++ projects/ppm/setup-vm.sh
@@ -4,17 +4,20 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 echo "adding shared folder NEED TO REMOVE REQUIREMENT IN ORIGINAL DEV_BOX"
 mkdir -p "$SRC_DIR/our-boxen"
 
-echo "adding vagrant box..."
-vagrant box add http://artrepo.daptiv.com:8081/artifactory/api/vagrant/vagrant-local/dev_ppm
+if ! vagrant box list | grep -q dev_ppm ; then
+  echo "adding vagrant box..."
+  vagrant box add http://artrepo.daptiv.com:8081/artifactory/api/vagrant/vagrant-local/dev_ppm
+fi
 
 DEV_PPM_DIR="$SRC_DIR/dev_ppm"
 
-[ -d "$DEV_PPM_DIR" ] && rm -rf "$DEV_PPM_DIR"
 mkdir -p "$DEV_PPM_DIR"
 
-echo "copying Vagrantfile..."
-cd "$DEV_PPM_DIR"
-[ -f "$DEV_PPM_DIR/Vagrantfile" ] || cp "$DIR/Vagrantfile" "$DEV_PPM_DIR/Vagrantfile"
+if ! [ -f "$DEV_PPM_DIR/Vagrantfile" ]; then
+  echo "copying Vagrantfile..."
+  cp "$DIR/Vagrantfile" "$DEV_PPM_DIR/Vagrantfile"
+fi
 
 echo "running vagrant up..."
+cd "$DEV_PPM_DIR"
 vagrant up
