5349d82f771857ed40b4f8e75b79667d1a687eac
James Trinklein <james.trinklein@changepoint.com>
2017-09-21 13:02:57 -0700

    fix: updates to ppm setup, make nodenv first on path

--- projects/ppm/setup-vm.sh
+++ projects/ppm/setup-vm.sh
@@ -5,7 +5,7 @@ echo "adding shared folder NEED TO REMOVE REQUIREMENT IN ORIGINAL DEV_BOX"
 mkdir -p "$SRC_DIR/our-boxen"
 
 echo "checking virtualbox networks..."
-if VBoxManage natnetwork list | grep -q -E 'Name: +NatNetwork$' ; then
+if ! VBoxManage natnetwork list | grep -q -E 'Name: +NatNetwork$' ; then
     echo "setting up virtualbox nat network"
     vboxmanage natnetwork add --netname NatNetwork --network 10.0.2.0/24 --ipv6 off --enable
     vboxmanage natnetwork start --netname NatNetwork
@@ -25,7 +25,7 @@ cp -fv "$DIR/Vagrantfile" "$DEV_PPM_DIR/Vagrantfile"
 
 echo "checking vagrant id"
 if [ 1 == $(vboxmanage list vms | grep -c "dev_ppm") ] ; then
-    vmid="$( VBoxManage list vms | awk '/dev_ppm/ { print substring($2, 2, 36) }' )"
+    vmid="$( VBoxManage list vms | awk '/dev_ppm/ { print substr($2, 2, 36) }' )"
     echo "$vmid" > "$DEV_PPM_DIR/.vagrant/machines/default/virtualbox/id"
 fi
 
@@ -33,3 +33,6 @@ echo "running vagrant up..."
 cd "$DEV_PPM_DIR"
 vagrant up
 
+echo "set network adapter 1 to natnetwork..."
+vmid="$( VBoxManage list vms | awk '/dev_ppm/ { print substr($2, 2, 36) }' )"
+vboxmanage controlvm $vmid nic1 natnetwork NatNetwork
