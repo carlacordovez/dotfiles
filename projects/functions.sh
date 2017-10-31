d5b532ad3a804edc33b8b0cf415f073338d7eff6
James Trinklein <james.trinklein@changepoint.com>
2017-10-31 14:52:05 -0700

    update setup scripts (#14)
    * chore: cleanup pmdashboard setup script to use helper functions
    
    * chore: add more helper functions
    
    * chore: update dtb setup scripts
    
    * chore: update ppmspa setup scripts
    
    * chore: update ttm setup scripts

--- projects/functions.sh
+++ projects/functions.sh
@@ -8,3 +8,22 @@ clone-missing-repo() {
     git clone "git@github.com:daptiv/$repo.git" "$SRC_DIR/$repo"
   fi
 }
+
+strap-repo() {
+  repo="$1"
+  repo_dir="$SRC_DIR/$repo"
+
+  if [ -f "$repo_dir/strap.sh" ]; then
+  	echo "running strapfile..."
+  	bash "$repo_dir/strap.sh"
+  else
+  	echo "strap file not found in $REPO repo!"
+  fi
+}
+
+clone-and-strap-repo() {
+  repo="$1"
+
+  clone-missing-repo "$repo"
+  strap-repo "$repo"
+}
