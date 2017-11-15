158e6179a84489c4c8d760b5a39c22261f3de761
James Trinklein <james.trinklein@changepoint.com>
2017-11-15 14:19:33 -0800

    update-helper-functions (#17)
    * feat: add clone-with-options
    
    allows for more configuration after clone including:
    - strapping
    - configuring git flow
    
    * chore: update projects to use clone-with-options
    
    * feat: add project setup for react theme
    
    * chore: update mspa setup to be consistent
    
    * fix: log functions print each arg on new line
    
    * feat: add green log msg color
    
    * chore: update mspa logging to use multi-arg format
    
    * fix: remove unnecessary shift from clone-with-options

--- projects/functions.sh
+++ projects/functions.sh
@@ -1,5 +1,76 @@
 #!/usr/bin/env bash
 
+log-msg () {
+  clr_green='\033[92m'
+  clr_yellow='\033[93m'
+  clr_red='\033[91m'
+  clr_default='\033[39m'
+
+  while [ "$1" != '' ]; do
+    case $1 in
+      -g|--green)
+        echo -en "$clr_green"
+        ;;
+      -y|--yellow)
+        echo -en "$clr_yellow"
+        ;;
+      -r|--red)
+        echo -en "$clr_red"
+        ;;
+      -d|--default)
+        echo -e "$clr_default"
+        ;;
+      *)
+        echo -e "$1"
+        ;;
+    esac
+    shift
+  done
+  echo -e "$clr_default"
+}
+
+log-warn() {
+  first=$1
+  shift
+  log-msg --yellow "WARNING: $first" $*
+}
+
+init-git-flow-for-repo() {
+  repo="$1"
+  repo_dir="$SRC_DIR/$repo"
+  config_file="$repo_dir/.git/config"
+
+  if [ -f "$config_file" ]; then
+  	if ! grep -q '\[gitflow' $config_file ; then
+      log-msg "setting up git flow for $repo..."
+      tee -a "$config_file" <<EOF
+[gitflow "branch"]
+  master = master
+  develop = dev
+[gitflow "prefix"]
+  feature = feature/
+  bugfix = bugfix/
+  release = release/
+  hotfix = hotfix/
+  support = support/
+  versiontag =
+[gitflow "path"]
+  hooks = ${repo_dir}/.git/hooks
+[gitflow "hotfix.finish"]
+  push = true
+[gitflow "release.finish"]
+  push = true
+[gitflow "feature.finish"]
+  push = true
+EOF
+    else
+      log-warn "git flow already configured for $repo!"
+    fi
+  else
+  	log-warn "git config file '$config_file' not found!"
+  fi
+}
+
 clone-missing-repo() {
   repo="$1"
 
@@ -21,9 +92,32 @@ strap-repo() {
   fi
 }
 
+clone-with-options() {
+  #repo should be last argument
+  repo="${@: -1}"
+
+  clone-missing-repo "$repo"
+
+  while [ "$1" != '' ]; do
+    case "$1" in
+      --strap)
+        strap-repo "$repo"
+        ;;
+      --git-flow)
+        init-git-flow-for-repo "$repo"
+        ;;
+      *)
+        #ignore unrecognized options
+        ;;
+    esac
+    shift
+  done
+}
+
+
 clone-and-strap-repo() {
   repo="$1"
 
-  clone-missing-repo "$repo"
-  strap-repo "$repo"
+  log-warn 'clone-and-strap-repo is deprecated, switch to: `clone-with-options --strap`'
+  clone-with-options "$repo" --strap
 }
